71e9d961c2fcdfce6efc73f3d872b97d8c9f0f28
SilentDefault <silentdefault@gmail.com>
2020-12-21 23:17:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 const ChromecastAPI = require('chromecast-api');
 var CronJob = require('cron').CronJob;
 
-const client = new ChromecastAPI();
-
 function runChime() {
+	const client = new ChromecastAPI();
 	client.on('device', function (device) {
 		if (device.friendlyName == 'Mini') {
-			var mediaURL =
-				'https://docs.google.com/uc?id=1FEhT6cr3UvaNnrg9qv7KDqHeitszRg6N';
-			device.play(mediaURL, function (err) {
-				if (!err) {
-                    console.log("chime start");
-                }
-            });
-            device.on('finished', function (){
-                device.close();
-                console.log("chime end");
-            });
+			console.log('Shiming');
+			device.play(
+				'https://docs.google.com/uc?id=1FEhT6cr3UvaNnrg9qv7KDqHeitszRg6N',
+				function (err) {
+					if (!err) {
+						console.log('chime start');
+					}
+				}
+			);
+			device.on('finished', function () {
+				device.close();
+				console.log('chime end');
+			});
 		}
 	});
 }
-
 var job = new CronJob(
-	'0 9/22 * * *',
-	runChime(),
+	'* 9-22 * * *',
+	function () {
+		runChime();
+	},
 	null,
 	true,
 	'America/Chicago'
